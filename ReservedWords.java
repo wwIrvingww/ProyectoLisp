374d158f33c894d28e64000c2db0896c2bdf9fbf
PabloAlvarez22 <pabloalv567@gmail.com>
2023-03-09 10:02:16 -0600

    Palabras Reservadas

---
+++ ReservedWords.java
@@ -0,0 +1,31 @@
+//Clase de las palabras claves de lisp
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ReservedWords {
+
+	private ArrayList<String> ReservedWords = new ArrayList<String>();
+	    
+	
+	public ReservedWords() {
+		ReservedWords = new ArrayList<String>();
+		ReservedWords.add("quote");
+		ReservedWords.add("defun");
+		ReservedWords.add("equals");
+		ReservedWords.add("print");
+		ReservedWords.add("setq");
+		ReservedWords.add("atom");
+		ReservedWords.add("list");
+		ReservedWords.add("cond");
+		ReservedWords.add("+");
+		ReservedWords.add("-");
+		ReservedWords.add("*");
+		ReservedWords.add("/");
+		ReservedWords.add("'");
+		ReservedWords.add("(");
+		ReservedWords.add(")");
+		ReservedWords.add("<");
+		ReservedWords.add(">");
+	}
+
+}
