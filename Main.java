fc1e199de258af76a3f65bf120e3fe989b05197f
wwIrvingww <mor22781@guvg.edu.gt>
2023-03-14 22:08:38 -0600

    Se creo el metodo getElementsInsideParentheses el cual obtiene todo lo que esta entre paraentesis de una LINKEDLIST Se creo que metodo spliAarraylist para separar un Sring en un arraylist

--- Main.java
+++ Main.java
@@ -10,16 +10,38 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) {
 
-        ArrayList<String> input = new ArrayList<>();
+        /**ArrayList<String> input = new ArrayList<>();
         input.add("+");
         input.add("2");
         input.add("3");
         input.add("4");
 
         int result = Arithmetic.evaluate(input);
-        System.out.println(result); // Imprime "9.0"
+        System.out.println(result); // Imprime "9.0"*/
+
+
+        LinkedList<String> instruccions;
+        String path ="C:\\Users\\USUARIO\\OneDrive\\UVG\\Clases\\Tercer Semestre\\Estructura de datos\\Codes\\ProyectoLisp\\code.txt";
+        Reader reader = new Reader();
+        ArrayList<String> arrayList = Reader.readFile(path);
+        instruccions = reader.convertArrayList(arrayList);
+
+        ArrayList<String> parentesis = Worker.getElementsInsideParentheses(instruccions);
+        /**for (String a: parentesis){
+            System.out.println(a);
+        }*/
+        System.out.println(parentesis.size());
+
+
+
+
+
 
     }
+
+
+
+
         /**
     public static List<Object> list(Object... elements) {
         LinkedList<Object> list = new LinkedList<>();
--- Worker.java
+++ Worker.java
@@ -2,7 +2,10 @@
  * Clase que se encarga de gestionar todas las operaciones y/o procesos, es decir este es el controlador
  */
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
+
 public class Worker {
 
     LinkedList<String> instruccions;
@@ -16,21 +19,59 @@ public class Worker {
 
     //------------------------------------------------------------------//
     public void work() {
-
-        for (String string : instruccions) {
-            if (reservedWords.getReservedWords().contains(string)) {
-                switch (string){
-                    case "+":
-
-                }
-
-
-
-            }
-        }
+            getElementsInsideParentheses(instruccions);
 
 
     }
+
+    public static ArrayList<String> getElementsInsideParentheses(LinkedList<String> linkedList) {
+        ArrayList<String> elementsInParentheses = new ArrayList<>();
+        boolean isInParentheses = false;
+
+        Iterator<String> iterator = linkedList.iterator();
+        while (iterator.hasNext()) {
+            String element = iterator.next();
+            if (element.contains("(")) {
+                isInParentheses = true;
+                String[] parts = element.split("\\(");
+                if (parts.length > 1) {
+                    String insideParentheses = parts[1];
+                    String[] insideParts = insideParentheses.substring(0, insideParentheses.length() - 1).split(",");
+                    for (String insidePart : insideParts) {
+                        elementsInParentheses.add(insidePart.trim());
+                    }
+                }
+            } else if (element.contains(")")) {
+                isInParentheses = false;
+            } else if (isInParentheses) {
+                String[] insideParts = element.split(",");
+                for (String insidePart : insideParts) {
+                    elementsInParentheses.add(insidePart.trim());
+                }
+            }
+        }
+        elementsInParentheses = splitAarraylist(elementsInParentheses);
+
+        return elementsInParentheses;
+    }
+
+    public static ArrayList<String> splitAarraylist(ArrayList<String> lista) {
+        String str = lista.get(0);
+        char[] chars = str.toCharArray();
+        ArrayList<String> resultado = new ArrayList<String>();
+
+        for (char c : chars) {
+            resultado.add(Character.toString(c));
+        }
+
+        return resultado;
+    }
+
+
+
+
+
+
 
 
 }
